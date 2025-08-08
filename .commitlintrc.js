5752954fea95ee0417e47efbeb7581e28d04e8fd
Jerome Brown <Jerome@SoliDeoGloria.tech>
2025-08-08 16:43:44 +1200

    ci(commitlint): ignore dependabot commit messages

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -29,5 +29,6 @@ module.exports = {
     'body-max-line-length': [2, 'always', 120],
     'footer-leading-blank': [1, 'always'],
     'footer-max-line-length': [2, 'always', 100]
-  }
+  },
+  ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
 }
